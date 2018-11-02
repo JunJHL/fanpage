c16065d4e75f77a74ab797ebb7035fe929be944f
Jesse <junjiejesse@gmail.com>
2018-11-02 07:29:16 +0000

    newest

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // JavaScript File
 /*global $*/
 
-$('body').click(function(){
-    
+$(".button").click(function(){
+    $(".navbar").hide();
+    $(".button").text("||");  
+    $(".main-img").attr("src","https://i.pinimg.com/originals/e2/54/2e/e2542e0c3de36df66a27d2d1b23172c5.jpg");
 });
-
